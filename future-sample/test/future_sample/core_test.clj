89b8bcd7e8c1b9c91b780666258ac44e94334333
eunmin <telepopsound@gmail.com>
2016-11-09 01:02:22 +0900

    예제 추가

---
+++ future-sample/test/future_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns future-sample.core-test
+  (:require [clojure.test :refer :all]
+            [future-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
